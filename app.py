674fe3c14f08fe93e5ef1089fd5fb2625fc1866f
prasant adhikari <pa1038@nyu.edu>
2019-12-03 14:29:27 -0500

    Store users in database

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # library imports
 from flask import Flask, render_template, redirect, request
-from flask_login import LoginManager, login_required, current_user
+from flask_login import LoginManager, login_required, current_user, login_user
 from os import urandom
 from subprocess import Popen, PIPE, STDOUT
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # imports from local files
 from models import User
@@ -15,11 +16,17 @@ app.config['SECRET_KEY'] = urandom(32)
 login = LoginManager(app)
 login.login_view = 'login'
 login.session_protection = "strong"
-users = {} #our make-do database for users
+
+# setup sqlite database for users
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from models import db
+db.init_app(app)
 
 @login.user_loader
-def load_user(uname):
-    return users.get(uname)
+def load_user(user_id):
+    with app.app_context():
+        return User.query.get(int(user_id))
 
 @app.after_request
 def apply_caching(response):
@@ -48,11 +55,14 @@ def register():
     message = None
     if form.validate_on_submit():
         uname, pword, twofa = get_data(request)
-        if uname in users.keys():
-            message = 'Registration failure! Username taken!'
-        else:
-            users[uname] = User(uname, pword, twofa)
-            message = 'Registration success! Login to continue'
+        with app.app_context():
+            if User.query.filter(User.uname==uname).first() is not None:
+                message = 'Registration failure! Username taken!'
+            else:
+                user = User(uname, pword, twofa)
+                db.session.add(user)
+                db.session.commit()
+                message = 'Registration success! Login to continue'
     elif request.method == 'POST':
         message = 'Registration failure!'
     return render_template('register.html', form=form, message=message)
@@ -60,13 +70,19 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = UserForm()
-    message = None
+    message = None if request.method == 'GET' else 'Incorrect username/password'
     if form.validate_on_submit():
         uname, pword, twofa = get_data(request)
-        if uname not in users.keys():
-            message = "Incorrect username/password"
-        else:
-            message = users[uname].authenticate(uname, pword, twofa)
+        with app.app_context():
+            user = User.query.filter(User.uname == uname).first()
+            if user is not None:
+                if check_password_hash(user.pword, pword) and user.twofa == twofa:
+                    user.is_authenticated = True
+                    db.session.commit()
+                    login_user(user)
+                    message = 'Login success'
+                elif user.twofa != twofa:
+                    message = 'Two-factor failure!'
     return render_template('login.html', form=form, message=message)
 
 @app.route('/spell_check', methods=['GET', 'POST'])
@@ -88,6 +104,7 @@ def spell_check():
 
     return render_template('spell_check.html', \
             form=form, textout=textout,misspelled=misspelled)
+
 
 if __name__=='__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,28 +1,28 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user
 
-class User:
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uname = db.Column(db.String(120), index=True, unique=True)
+    pword = db.Column(db.String(120), nullable=False)
+    twofa =  db.Column(db.String(120), nullable=True)
+    is_active =  True
+    is_anonymous = False
+    is_authenticated = True
+
     def __init__(self, uname=None, pword=None, twofa=None):
         self.uname = uname
         self.pword = generate_password_hash(pword, \
                         method='pbkdf2:sha256:150000', salt_length=16)
         self.twofa = twofa
-        self.is_authenticated = False
-        self.is_active = True
-        self.is_anonymous = False
 
     def get_id(self):
-        return self.uname
-
-    def authenticate(self, uname, pword, twofa):
-        if (self.uname != uname or not check_password_hash(self.pword, pword)):
-            return 'Incorrect username/password!'
-        if (self.twofa != twofa):
-            return 'Two-factor failure!'
-        self.is_authenticated = True
-        login_user(self)
-        return 'Login success'
+        return self.id
 
     def __str__(self):
         return "uname : {}\n pword: {}\n  2fa: {}\n authenticated: {}\n"\
                 .format(self.uname, self.pword, self.twofa, self.authenticated)
+
